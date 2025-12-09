3e785ffc076b911d2c2066a6649f88eba7d87710
Juan Estevez Castillo <jd.estevezcastillo@gmail.com>
2025-12-09 12:28:02 +0100

    chore: Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="heimr-ai",
-    version="0.1.0",
+    version="0.1.1",
     author="Juan Estevez Castillo",
     author_email="jd.estevezcastillo@gmail.com",
     description="AI-Powered Load Test Analysis & Root Cause Explanation",
